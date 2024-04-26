38726c738ee5a5f877b22e5abac89f44180b8668
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-04-26 11:01:13 -0300

    criação das branches

--- projeto 365/src/App.jsx
+++ projeto 365/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    // alert('funcionando');
+    fetch("http://localhost:3000/usuarios")
+      .then(response => response.json())
+      .then(dados => setUsuarios(dados))
+      .catch(erro => console.log(erro))
+
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Meu App</h1>
+
+      {!!usuarios && usuarios.map(usuario => (
+        <h2 key={usuario.id}> {usuario.nome}</h2>
+
+      ))}
     </>
   )
 }
