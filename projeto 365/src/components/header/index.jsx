44832c4c16b6ce6529fded906f77c2f79e44f759
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-05-01 14:53:05 -0300

    adição da pasta components com footer e header em estado avançado

---
+++ projeto 365/src/components/header/index.jsx
@@ -0,0 +1,22 @@
+import styles from "./index.module.css"
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className={styles.header}>
+             <h4>Fitness PlaceX</h4>
+            <nav className={styles.nav}>
+           
+                <Link to="/" className={styles.link}>Login/Cadastro</Link>
+
+                <Link to="/dashboard" className={styles.link}>DashBoard</Link>
+               
+                <Link to="/lista" className={styles.link}>Encontrar Locais</Link>
+                
+                <Link to="/cadastro" className={styles.link}>Cadastrar Locais</Link>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
