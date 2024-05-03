39129eb735331c81796f00e12cca786cc7baa8b4
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-05-03 12:33:07 -0300

    adição de botões editar para chamar ppagina de cadastro de locais

--- projeto 365/src/pages/lista/index.jsx
+++ projeto 365/src/pages/lista/index.jsx
@@ -1,19 +1,38 @@
 import { UsuariosContext } from '../../context/UsuariosContext';
 import { useContext, useEffect, useState } from 'react';
 import styles from "./index.module.css"
+import { useNavigate } from 'react-router-dom';
+
+function PaginaLista() {
+
+    const { usuarios, cadastrarUsuario, editarUsuario, apagarUsuario, lerUsuariosPorId,
+        mostrarCadLocal, setCadLocal, mostrarEdicaoLocal, setMostrarEdicaoLocal
+    } = useContext(UsuariosContext);
+
+    const navigate = useNavigate();
 
 
-function PaginaLista(){
 
-    const { usuarios, cadastrarUsuario, editarUsuario, apagarUsuario, lerUsuariosPorId } = useContext(UsuariosContext);
+    function voltarCadastroLocais() {
+        setCadLocal(false)
+        setMostrarEdicaoLocal(true)
+        navigate('/cadastro');
+        //guardar o do local
+        //faz lerLocaisporId(id) para preencher o formulário com informações do usuario ativo
 
 
+        return (
+            <>
+                <div className={styles.container}>
+                    <h1>Lista de Locais</h1>
+                </div>
 
-    return(
-        <div className={styles.container}>
-        <h1>Lista de Locais</h1>
-        </div>
-    )
+                <button onClick={voltarCadastroLocais}>Editar</button>
+                <button>Excluir</button>
+
+            </>
+        )
+    }
 }
 
-export default PaginaLista;
+    export default PaginaLista;
